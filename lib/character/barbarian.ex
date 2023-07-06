33bc7839b76adca20dbb6264a3dbc6fbbc4e9313
shaler7882 <shawnhaler@yahoo.com>
2023-07-06 14:35:14 -0700

    refactor

--- lib/character/barbarian.ex
+++ lib/character/barbarian.ex
@@ -1,9 +1,7 @@
 defimpl Character, for: BattleMap.Barbarian do
   def can_attack?(_character, {org1, org2}, {tar1, tar2}) do
-    if(abs(org1 - tar1) <= 2 and abs(org2 - tar2) <= 2) do
-      true
-    else
-      false
-    end
+    diff1 = abs(org1 - tar1)
+    diff2 = abs(org2 - tar2)
+    diff1 <= 2 && diff2 <= 2
   end
 end
--- lib/character/wizard.ex
+++ lib/character/wizard.ex
@@ -1,9 +1,7 @@
 defimpl Character, for: BattleMap.Wizard do
   def can_attack?(_character, {org1, org2}, {tar1, tar2}) do
-    if org1 == tar1 or org2 == tar2 or abs(org1 - tar1) == abs(org2 - tar2) do
-      true
-    else
-      false
-    end
+    diff1 = abs(org1 - tar1)
+    diff2 = abs(org2 - tar2)
+    org1 == tar1 || org2 == tar2 || diff1 == diff2
   end
 end
